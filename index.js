84a494ea74c5a9db005359ff44a3692cf7365801
mostafa-meerzad <71199539+mostafa-meerzad@users.noreply.github.com>
2023-09-28 10:28:45 +0430

    complete the url shortening challenge

--- index.js
+++ index.js
@@ -1,24 +1,54 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
-
+const dns = require("dns");
+const { URL } = require("url");
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
+let shorts = [];
+
+app.post("/api/shorturl", (req, res) => {
+  try {
+    const parsedUrl = new URL(req.body.url);
+    const hostName = parsedUrl.hostname;
+    dns.lookup(hostName, (err, address, family) => {
+      if (err) return res.json({ error: "invalid url" });
+
+      let urlId = shorts.length + 1;
+      shorts.push({ id: urlId, url: req.body.url });
+
+      return res.json({ original_url: req.body.url, short_url: urlId });
+    });
+  } catch (err) {
+    return res.json({ error: "invalid url" });
+  }
+});
+
+app.get("/api/shorturl/:id", (req, res) => {
+  const short = shorts.find((item) => item.id === parseInt(req.params.id));
+  console.log(short);
+
+  if (!short) return res.json({ error: "invalid url" });
+
+  return res.redirect(short.url);
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
